b6941b6ffcb6d781825ac18c4f85d063c84e7472
こんぺい党 <1125okitasougo@gmail.com>
2023-02-12 17:53:48 +0900

    a

---
+++ javascript/loader.js
@@ -0,0 +1,10 @@
+$(function () {
+  function end_loader() {
+    $('.loader').fadeOut(800);
+  }
+  $(window).on('load', function () {
+    setTimeout(function () {
+      end_loader();
+    }, 3000)
+  })
+})
