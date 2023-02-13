25a31d5acfd4a5ed60ce9b9bbfe12ea57d78fbd0
こんぺい党 <1125okitasougo@gmail.com>
2023-02-13 21:11:23 +0900

    a

--- javascript/loader.js
+++ javascript/loader.js
@@ -1,11 +1,15 @@
-$(function (){
-  function end_loader() {
-    $('.loader').fadeOut(800);
-  }
-  $(window).on('load', function () {
-    setTimeout(function () {
-      end_loader();
-    setTimeout('stopload ()',1000);
-    }, 2000)
-  })
-})
+var loading2_on = function() {
+  $(document.body).append("<div id=\"blind\"><span class=\"loading22\"></span></div>");
+};
+var loading2_off = function() {
+  $("#blind").remove();
+};
+var hoge2 = function() {
+  loading2_on();
+  $.ajax( {
+      "url": "",
+      "complete": function() {
+          loading2_off();
+      }
+  } );
+};　
