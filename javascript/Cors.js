3112febcfc99e5cc67b0cd1bcc9e87d36ab88d28
こんぺい党 <1125okitasougo@gmail.com>
2023-02-17 12:31:48 +0900

    Delete Cors.js

--- javascript/Cors.js
+++
@@ -1,8 +0,0 @@
-router.get('/', (req, res) => {
-    try {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-        res.status(201).send('ok')
-    } catch (error) {
-        console.error(error)
-    }
-})
